6dc3833dbcd9af5d4c500116f217ff27ed47a737
oznursayin <oznurozturk1@hotmail.com>
2025-06-23 23:11:58 +0300

    v_01

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+"use client"
+import { useState } from "react"
+import ImageCreate from "@/components/ImageCreate"
+
+const App = () => {
+  return (
+    <>
+      <ImageCreate />
+    </>
+  )
+}
+export default App
---
+++ src/components/ImageCreate.js
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+const ImageCreate = () => {
+  const [images, setImages] = useState([])
+
+  const handleNewImage = () => {
+    const newImage = {
+      id: crypto.randomUUID(),
+      url: `https://picsum.photos/seed/${crypto.randomUUID()}/300/200`,
+      iconSize: 16,
+    }
+    const newImages = [...images, newImage]
+    setImages(newImages)
+  }
+
+  const handleIconClick = (id) => {
+    setImages((images) =>
+      images.map((image) =>
+        image.id === id
+          ? {
+              ...image,
+              iconSize:
+                image.iconSize < 30 ? image.iconSize + 5 : image.iconSize,
+            }
+          : image
+      )
+    )
+  }
+
+  return (
+    <div>
+      <button onClick={handleNewImage}>Resim Ekle</button>
+      <div className="images-container">
+        {images.map((image) => (
+          <div className="image-container" key={image.id}>
+            <img src={image.url} alt="" />
+            <div
+              onClick={() => handleIconClick(image.id)}
+              style={{ fontSize: `${image.iconSize}px` }}
+            >
+              👍
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+export default ImageCreate
